8443da7f35ed93922a02afc0274d601aa87af0b4
BuiThanhLong <lbui21199@gmail.com>
2022-01-05 20:13:59 +0700

    Initialize the PostController

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,19 @@
+import { Handler, Response } from "express"
+import { request } from "http"
+import { AuthenticatedRequest } from "../types"
+
+export const create: Handler = async (request: AuthenticatedRequest, response: Response) => {
+    // TODO
+}
+
+export const search = async (request: AuthenticatedRequest, response: Response) => {
+    // TODO
+}
+
+export const update = async (request: AuthenticatedRequest, response: Response) => {
+    // TODO
+}
+
+export const remove = async (request: AuthenticatedRequest, response: Response) => {
+    // TODO
+}
