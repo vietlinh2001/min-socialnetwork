716c7f13cb870f6c98c0770904a995c54db69567
Viet Linh <vietlinhdt2001@gmail.com>
2022-01-05 19:25:35 +0700

    add entity

--- src/routing.ts
+++ src/routing.ts
@@ -2,6 +2,7 @@ import * as UserController from './controllers/UserController'
 import { Router }          from "express";
 import { body }            from "express-validator";
 import validate            from "./middlewares/validate";
+import requireAuthenticated from './middlewares/requireAuthenticate';
 
 const controller = (method) => (request, response, next) => {
   method(request, response, next).catch(error => next(error))
@@ -9,26 +10,8 @@ const controller = (method) => (request, response, next) => {
 
 export default (router: Router) => {
 
-  // TODO will add more route here
-  router.get('/user/:id', controller(UserController.detail))
+  router.use(requireAuthenticated)
 
-  router.put('/user/:id',
-    validate(
-      body('name').isString().isLength({min: 3, max: 25})
-    ),
-    controller(UserController.update)
-  )
-
-  router.delete('/user/:id', controller(UserController.remove))
-
-  router.get('/user', controller(UserController.search))
-
-  router.post('/user',
-    validate(
-      body('name').isString().isLength({min: 3, max: 25}),
-      body('email').isEmail()
-    ),
-    controller(UserController.create)
-  )
+  router.get('/user/me', controller(UserController.profile))
 }
 
